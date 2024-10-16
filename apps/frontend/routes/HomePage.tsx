051379b090d5cb724957186a3cb64186bfe5e104
jjmurphy1114 <jjmurphy1114@outlook.com>
2024-10-16 15:07:46 -0400

    restructured files, moved src into frontend, added router to App.tsx

---
+++ apps/frontend/routes/HomePage.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+
+export default function HomePage(){
+    return(
+        <div>
+            <h1>
+                ZM PARTIES BITCH
+            </h1>
+        </div>
+    )
+}
