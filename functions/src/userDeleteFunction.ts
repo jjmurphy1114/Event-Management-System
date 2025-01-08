541bd374071a21908e474777e644bf53e5e73cfd
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-01-08 01:41:43 -0500

    Fixed incorrect admin authentication; First working deployable version

--- functions/src/userDeleteFunction.ts
+++ functions/src/userDeleteFunction.ts
@@ -1,12 +1,8 @@
 import admin from "firebase-admin";
 import {onValueDeleted} from "firebase-functions/database";
-import * as serviceAccount from "./zm-parties-service-account-key.json";
 
 if(admin.apps.length === 0) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-        databaseURL: "https://zm-parties-2-default-rtdb.firebaseio.com/"
-    });
+    admin.initializeApp();
 }
 
 export const onUserDelete = onValueDeleted({
