0949b46aabf4c050cb7f1e1c7f8621c54b607901
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-01-11 22:51:03 -0500

    Slight mobile styling changes to HomePage.tsx

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -40,7 +40,7 @@ export default function HomePage() {
   };
 
   return (
-    <div className="h-screen w-screen bg-gradient-to-b justify-center items-top from-blue-50 to-gray-100 py-10 px-5 min-w-fit lg:px-[25%] pt-20 overflow-auto">
+    <div className="box-border h-screen w-screen bg-gradient-to-b justify-center items-top from-blue-50 to-gray-100 py-10 px-5 min-w-[420px] md:px-[10%] lg:px-[20%] pt-20 overflow-auto">
       <h1 className="text-4xl font-bold text-center w-full align-top text-indigo-600 mt-10 mb-10">ZM PARTIES</h1>
       <div className="flex w-full justify-center mx-auto bg-gradient-to-b from-blue-50 to-gray-100">
         <div className="bg-white shadow-md w-full item-center rounded-lg p-6">
